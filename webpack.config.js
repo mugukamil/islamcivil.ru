57c2e4e33ac638383f8957c9d5ef0c541ea9ba11
mugukamil <mugukamil@gmail.com>
2019-07-09 22:48:26 +0300

    adaptive

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,30 @@
-const webpack = require('webpack');
-const path = require('path');
-const config = require('./gulp/config');
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const webpack = require("webpack");
+const path = require("path");
+const {BundleAnalyzerPlugin} = require("webpack-bundle-analyzer");
+const config = require("./gulp/config");
 
 function createConfig(env) {
-  let isProduction,
-    webpackConfig;
+  let isProduction; let webpackConfig;
 
   if (env === undefined) {
     env = process.env.NODE_ENV;
   }
 
-  isProduction = env === 'production';
+  isProduction = env === "production";
 
   webpackConfig = {
-    mode: isProduction?'production':'development',
+    mode: isProduction ? "production" : "development",
     context: path.join(__dirname, config.src.js),
     entry: {
       // vendor: ['jquery'],
-      app: './app.js',
+      app: "./app.js"
     },
     output: {
       path: path.join(__dirname, config.dest.js),
-      filename: '[name].js',
-      publicPath: 'js/',
+      filename: "[name].js",
+      publicPath: "js/"
     },
-    devtool: isProduction ?
-      '#source-map' :
-      '#cheap-module-eval-source-map',
+    devtool: isProduction ? "#source-map" : "#cheap-module-eval-source-map",
     plugins: [
       // new webpack.optimize.CommonsChunkPlugin({
       //     name: 'vendor',
@@ -37,69 +34,95 @@ function createConfig(env) {
       new webpack.LoaderOptionsPlugin({
         options: {
           eslint: {
-            formatter: require('eslint-formatter-pretty')
+            formatter: require("eslint-formatter-pretty")
           }
         }
       }),
       new webpack.ProvidePlugin({
-        $: 'jquery',
-        jQuery: 'jquery',
-        'window.jQuery': 'jquery',
+        $: "jquery",
+        jQuery: "jquery",
+        "window.jQuery": "jquery"
       }),
       new webpack.NoEmitOnErrorsPlugin(),
 
       new BundleAnalyzerPlugin({
-        analyzerMode: 'static',
+        analyzerMode: "static",
         analyzerPort: 4000,
-        openAnalyzer: false,
-      }),
+        openAnalyzer: false
+      })
     ],
     resolve: {
-      extensions: ['.js'],
+      extensions: [".js"],
       alias: {
-        TweenLite: path.resolve('node_modules', 'gsap/src/uncompressed/TweenLite.js'),
-        TweenMax: path.resolve('node_modules', 'gsap/src/uncompressed/TweenMax.js'),
-        TimelineLite: path.resolve('node_modules', 'gsap/src/uncompressed/TimelineLite.js'),
-        TimelineMax: path.resolve('node_modules', 'gsap/src/uncompressed/TimelineMax.js'),
-        ScrollMagic: path.resolve('node_modules', 'scrollmagic/scrollmagic/uncompressed/ScrollMagic.js'),
-        'animation.gsap': path.resolve('node_modules', 'scrollmagic/scrollmagic/uncompressed/plugins/animation.gsap.js'),
-        'debug.addIndicators': path.resolve('node_modules', 'scrollmagic/scrollmagic/uncompressed/plugins/debug.addIndicators.js'),
-      },
+        TweenLite: path.resolve(
+          "node_modules",
+          "gsap/src/uncompressed/TweenLite.js"
+        ),
+        TweenMax: path.resolve(
+          "node_modules",
+          "gsap/src/uncompressed/TweenMax.js"
+        ),
+        TimelineLite: path.resolve(
+          "node_modules",
+          "gsap/src/uncompressed/TimelineLite.js"
+        ),
+        TimelineMax: path.resolve(
+          "node_modules",
+          "gsap/src/uncompressed/TimelineMax.js"
+        ),
+        ScrollMagic: path.resolve(
+          "node_modules",
+          "scrollmagic/scrollmagic/uncompressed/ScrollMagic.js"
+        ),
+        "animation.gsap": path.resolve(
+          "node_modules",
+          "scrollmagic/scrollmagic/uncompressed/plugins/animation.gsap.js"
+        ),
+        "debug.addIndicators": path.resolve(
+          "node_modules",
+          "scrollmagic/scrollmagic/uncompressed/plugins/debug.addIndicators.js"
+        )
+      }
     },
-    optimization :{
+    optimization: {
       minimize: isProduction
     },
     module: {
       rules: [
         {
-          enforce: 'pre',
+          enforce: "pre",
           test: /\.js$/,
-          exclude: [
-            path.resolve(__dirname, 'node_modules'),
-          ],
-          loader: 'eslint-loader',
+          exclude: [path.resolve(__dirname, "node_modules")],
+          loader: "eslint-loader",
           options: {
             fix: true,
             cache: true,
-            ignorePattern: __dirname + '/src/js/lib/'
+            ignorePattern: `${__dirname  }/src/js/lib/`
           }
-        }, {
-          test: /\.js$/,
-          loader: 'babel-loader',
-          exclude: [
-            path.resolve(__dirname, 'node_modules'),
-          ],
         },
-        { test: /\.(glsl|frag|vert)$/, loader: 'raw-loader', exclude: /node_modules/ },
-        { test: /\.(glsl|frag|vert)$/, loader: 'glslify-loader', exclude: /node_modules/ }
-        ],
-    },
+        {
+          test: /\.js$/,
+          loader: "babel-loader",
+          exclude: [path.resolve(__dirname, "node_modules")]
+        },
+        {
+          test: /\.(glsl|frag|vert)$/,
+          loader: "raw-loader",
+          exclude: /node_modules/
+        },
+        {
+          test: /\.(glsl|frag|vert)$/,
+          loader: "glslify-loader",
+          exclude: /node_modules/
+        }
+      ]
+    }
   };
 
   if (isProduction) {
     webpackConfig.plugins.push(
       new webpack.LoaderOptionsPlugin({
-        minimize: true,
+        minimize: true
       })
     );
   }
