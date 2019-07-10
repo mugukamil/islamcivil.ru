41cf0b56801e8d882425ae82a4a7010e97d8b284
mugukamil <mugukamil@gmail.com>
2019-07-10 20:48:56 +0300

    mobile

--- src/js/app.js
+++ src/js/app.js
@@ -1,65 +1,83 @@
 import $ from "jquery";
 import "slick-carousel";
 
-function scrollStarted() {
-  const start = 1;
+$(document).ready(function() {
+  const $thirdCat = $(".cat:nth-child(3)");
+  const thirdCatOffset = $thirdCat.offset().top;
+  const thirdCatHeight = $thirdCat.height();
 
-  if (start <= window.pageYOffset) {
-    document.body.classList.add("is-scrolled");
-  } else {
-    document.body.classList.remove("is-scrolled");
-  }
-}
-if (window.innerWidth > 300) {
-  window.addEventListener(
-    "scroll",
-    () => window.requestAnimationFrame(scrollStarted),
-    { passive: !0 }
-  );
-
-  scrollStarted();
-}
-
-$(".cols-slider").slick({
-  slidesToShow: 4,
-  slidesToScroll: 1,
-  prevArrow: `<button type='button' class='slick-prev'>
-    <svg width="12" height="22" viewBox="0 0 12 22" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path d="M9.15817 20.9165C9.69689 21.4998 10.6191 21.4981 11.1557 20.9129C11.6329 20.3926 11.6315 19.5933 11.1525 19.0746L4.94849 12.3569C4.24085 11.5907 4.24085 10.4093 4.94848 9.64308L11.1525 2.92538C11.6315 2.40671 11.6329 1.60742 11.1557 1.08707C10.6191 0.501853 9.69689 0.500229 9.15817 1.08355L1.25316 9.64308C0.545527 10.4093 0.545527 11.5907 1.25316 12.3569L9.15817 20.9165Z" fill="white"/>
-    </svg>
-  </button>`,
-  nextArrow: `<button type='button' class='slick-next'>
-    <svg width="12" height="22" viewBox="0 0 12 22" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path d="M9.15817 20.9165C9.69689 21.4998 10.6191 21.4981 11.1557 20.9129C11.6329 20.3926 11.6315 19.5933 11.1525 19.0746L4.94849 12.3569C4.24085 11.5907 4.24085 10.4093 4.94848 9.64308L11.1525 2.92538C11.6315 2.40671 11.6329 1.60742 11.1557 1.08707C10.6191 0.501853 9.69689 0.500229 9.15817 1.08355L1.25316 9.64308C0.545527 10.4093 0.545527 11.5907 1.25316 12.3569L9.15817 20.9165Z" fill="white"/>
-    </svg>
-  </button>`,
-  responsive: [
-    {
-      breakpoint: 1280,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 1
-      }
-    },
-    {
-      breakpoint: 768,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        arrows: false
-      }
-    }
-  ]
-});
-
-if (window.innerWidth < 768) {
-  $(".remembers__slider").slick({
-    infinite: false,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    dots: true,
-    arrows: false,
-    centerMode: true,
-    centerPadding: 30
+  $(".socials").css({
+    top: thirdCatOffset + thirdCatHeight
   });
-}
+
+  function scrollStarted() {
+    const start = 1;
+
+    if (start <= window.pageYOffset) {
+      document.body.classList.add("is-scrolled");
+    } else {
+      document.body.classList.remove("is-scrolled");
+    }
+  }
+  if (window.innerWidth > 300) {
+    window.addEventListener(
+      "scroll",
+      () => window.requestAnimationFrame(scrollStarted),
+      { passive: !0 }
+    );
+
+    scrollStarted();
+  }
+
+  $(".video-slider__in").slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    prevArrow: `<button type='button' class='slick-prev'>
+    <svg width="12" height="22" viewBox="0 0 12 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M9.15817 20.9165C9.69689 21.4998 10.6191 21.4981 11.1557 20.9129C11.6329 20.3926 11.6315 19.5933 11.1525 19.0746L4.94849 12.3569C4.24085 11.5907 4.24085 10.4093 4.94848 9.64308L11.1525 2.92538C11.6315 2.40671 11.6329 1.60742 11.1557 1.08707C10.6191 0.501853 9.69689 0.500229 9.15817 1.08355L1.25316 9.64308C0.545527 10.4093 0.545527 11.5907 1.25316 12.3569L9.15817 20.9165Z" fill="white"/>
+    </svg>
+  </button>`,
+    nextArrow: `<button type='button' class='slick-next'>
+    <svg width="12" height="22" viewBox="0 0 12 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M9.15817 20.9165C9.69689 21.4998 10.6191 21.4981 11.1557 20.9129C11.6329 20.3926 11.6315 19.5933 11.1525 19.0746L4.94849 12.3569C4.24085 11.5907 4.24085 10.4093 4.94848 9.64308L11.1525 2.92538C11.6315 2.40671 11.6329 1.60742 11.1557 1.08707C10.6191 0.501853 9.69689 0.500229 9.15817 1.08355L1.25316 9.64308C0.545527 10.4093 0.545527 11.5907 1.25316 12.3569L9.15817 20.9165Z" fill="white"/>
+    </svg>
+  </button>`,
+    responsive: [
+      {
+        breakpoint: 1280,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          arrows: false,
+          asNavFor: ".video-slider__nav"
+        }
+      }
+    ]
+  });
+
+  $(".video-slider__nav").slick({
+    asNavFor: ".video-slider__in",
+    centerMode: true,
+    centerPadding: "20px",
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    arrows: false
+  });
+
+  if (window.innerWidth < 768) {
+    $(".remembers__slider").slick({
+      infinite: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      dots: true,
+      arrows: false
+    });
+  }
+});
